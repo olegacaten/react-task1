9a2fc6087667831ac9654397e6c25beac89b4b05
Oleg Makarov <olegmakarovach.job@gmail.com>
2024-07-08 01:13:10 +0300

    feat:Updated functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,136 @@
 import React from 'react';
 import './App.css';
 
-interface PokemonSprites {
-  front_default: string | null;
-}
 
 interface Pokemon {
   height: number;
-  order: number;
+  id: number;
   name: string;
-  sprites: PokemonSprites;
+  sprites: {
+    front_default: string;
+  };
+  types: {
+    slot: number;
+    type: {
+      name: string;
+    };
+  }[];
 }
 interface InterfaceData {
   count: number;
   next: string;
-  previous: string | null;
+  previous: string;
   results: { name: string; url: string }[];
 }
+
 interface MyState {
-  SearchWord: string;
-  DataFetched: InterfaceData[] | null;
-  DidEverythingLoaded: boolean;
+  SearchWord:string;
+  PokemonList: InterfaceData | null;
+  DetailedPokemon: Pokemon[] | null; 
+  isLoading: boolean;
+  fetchingDetailed: boolean;
+
 }
 
+
 class App extends React.Component<object, MyState> {
+
   state: MyState = {
     SearchWord: '',
-    DataFetched: null,
-    DidEverythingLoaded: false,
+    PokemonList: null,
+    DetailedPokemon: null, 
+    isLoading: true,
+    fetchingDetailed: false,
   };
 
-  componentDidMount() {
-    this.FindSearchWord();
-  }
 
   InputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.setState((prevState) => {
       const newValue = e.target.value;
       if (prevState.SearchWord !== newValue) {
-        console.log('Previous State: ', prevState.SearchWord);
-        console.log('New Value: ', newValue);
+        console.log('Previous SearchWord: ', prevState.SearchWord);
+        console.log('New SearchWord: ', newValue);
       }
       return { SearchWord: newValue };
     });
   };
 
-  FindSearchWord = () => {
-    const BaseUrl = 'https://pokeapi.co/api/v2/pokemon';
-    const SearchWordExist = this.state.SearchWord ? `/${this.state.SearchWord}` : '';
-    const url = `${BaseUrl}${SearchWordExist}?offset=0&limit=20`;
+  fetchPokemonData = async () => {
+    this.setState({ isLoading: true }); // Show loading indicator
+    try {
+      let data;
+      if (this.state.SearchWord) {
+        // Fetch single Pokemon by name
+        const response = await fetch(
+          `https://pokeapi.co/api/v2/pokemon/${this.state.SearchWord}`
+        );
+        data = await response.json();
+        this.setState({ DetailedPokemon: [data], isLoading: false }); // Set isLoading to false after fetching single Pokemon
+      } else {
+        // Fetch list of Pokemon
+        let apiUrl = 'https://pokeapi.co/api/v2/pokemon?offset=0&limit=6'; // Default URL
+        if (this.state.PokemonList && (this.state.PokemonList.next || this.state.PokemonList.previous)) {
 
-    fetch(url)
-      .then((response) => response.json())
-      .then((pokemonList: InterfaceData) => {
-        const detailedDataPromises = pokemonList.results.map((pokemon) => fetch(pokemon.url).then((res) => res.json()));
-        Promise.all(detailedDataPromises).then((details) => {
-          this.setState({ DataFetched: details, DidEverythingLoaded: true });
-        });
-        console.log(url);
-        console.log(this.state.DataFetched);
-      });
+          apiUrl = this.state.PokemonList.next || this.state.PokemonList.previous; 
+        }
+        const response = await fetch(apiUrl);
+        data = await response.json();
+        this.setState({ PokemonList: data });
+
+        // Fetch detailed data after fetching the initial list
+        if (!this.state.fetchingDetailed) {
+          this.setState({ fetchingDetailed: true }, () => { 
+            this.fetchDetailedPokemon(); 
+          });
+        }
+      }
+      console.log('Successful fetchPokemonData');
+    } catch (error) {
+      console.error('Error fetching Pokemon data:', error);
+      this.setState({ DetailedPokemon: null, isLoading: false });
+    }
   };
+
+
+  fetchDetailedPokemon = async () => {
+    if (!this.state.PokemonList) {
+      return;
+    }
+    try {
+      const promises = this.state.PokemonList.results.map((pokemon) =>
+        fetch(pokemon.url).then((res) => res.json())
+      );
+
+      const detailedPokemonData: Pokemon[] = await Promise.all(promises);
+      this.setState({ DetailedPokemon: detailedPokemonData, isLoading: false, fetchingDetailed: false }); 
+      console.log('Gathered Detailed info');
+      console.log(detailedPokemonData);
+    } catch (error) {
+      console.error("Error fetching detailed Pokemon:", error);
+      this.setState({ isLoading: false, fetchingDetailed: false }); 
+    }
+  };
+
+  componentDidMount() {
+    this.fetchPokemonData();
+    console.log('componentDidMount');
+  }
+
+  handlePagination = (nextPage: boolean) => {
+    if (this.state.PokemonList) {
+      const url = nextPage ? this.state.PokemonList.next : this.state.PokemonList.previous; 
+      if (url) {
+        this.fetchPokemonData();
+      }
+    }
+  };
+
+
 
   render() {
     return (
       <div className="Container">
+           {this.state.isLoading && <div>Loading...</div>}
         <div className="Container__search-container">
           <input
             type="text"
@@ -73,28 +139,71 @@ class App extends React.Component<object, MyState> {
             placeholder="Type your search word"
             className="Container__search-container__input"
           />
-          <button onClick={this.FindSearchWord} className="Container__search-container__button">
+          <button onClick={this.fetchPokemonData} className="Container__search-container__button">
             Search
           </button>
         </div>
+        <div className="Container__pokedex-result">
+          {/* Check if a single Pokemon is being displayed */}
+          {this.state.DetailedPokemon && this.state.DetailedPokemon.length === 1 ? (
+            <div className="Container__pokedex-result__card">
+              {this.state.DetailedPokemon[0].id ? `"${this.state.DetailedPokemon[0].id}"` : 'id not available'}
+              {this.state.DetailedPokemon[0].types.length > 0 ? (
+                this.state.DetailedPokemon[0].types.map((type) => type.type.name).join(', ')
+              ) : (
+                'No Type Available'
+              )}
+              {this.state.DetailedPokemon[0].name ? `"${this.state.DetailedPokemon[0].name}"` : 'Name not available'}
+              {this.state.DetailedPokemon[0].sprites && this.state.DetailedPokemon[0].sprites.front_default ? (
+                <img src={this.state.DetailedPokemon[0].sprites.front_default} alt={this.state.DetailedPokemon[0].name} />
+              ) : (
+                'No Image Available'
+              )}
+            </div>
+          ) : (
+            <>
+              {/* If multiple Pokemon, show pagination buttons */}
+            
 
-        {this.state.DataFetched && this.state.DataFetched.results
-          ? this.state.DataFetched.results.map((pokemon: Pokemon) => {
-              console.log(pokemon); // Log the entire pokemon object to inspect its structure
-              return (
-                <div key={pokemon.order}>
-                  {pokemon.order ? `"${pokemon.order}"` : 'Order not available'}
-                  {pokemon.height ? `"${pokemon.height}"` : 'Height not available'}
-                  {pokemon.name ? `"${pokemon.name}"` : 'Name not available'}
-                  {pokemon.sprites && pokemon.sprites.front_default ? (
-                    <img src={pokemon.sprites.front_default} alt={pokemon.name} />
-                  ) : (
-                    'No Image Available'
+              {/* Display the Pokemon list */}
+              {this.state.DetailedPokemon ? (
+                this.state.DetailedPokemon.map((pokemon: Pokemon, index) => {
+                  return (
+                    <div key={index} className="Container__pokedex-result__card">
+                      {pokemon.id ? `"${pokemon.id}"` : 'id not available'}
+                      {pokemon.types.length > 0 ? (
+                        pokemon.types.map((type) => type.type.name).join(', ')
+                      ) : (
+                        'No Type Available'
+                      )}
+                      {pokemon.name ? `"${pokemon.name}"` : 'Name not available'}
+                      {pokemon.sprites && pokemon.sprites.front_default ? (
+                        <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+                      ) : (
+                        'No Image Available'
+                      )}
+                    </div>
+                  );
+                })
+              ) : (
+                'No Data Available'
+              )}
+            </>
+          )}
+
+{this.state.PokemonList && (
+                <>
+                  {this.state.PokemonList.previous && (
+                    <button onClick={() => this.handlePagination(false)}>Previous</button>
                   )}
-                </div>
-              );
-            })
-          : 'Loading or No Data Available'}
+                  {this.state.PokemonList.next && (
+                    <button onClick={() => this.handlePagination(true)}>Next</button>
+                  )}
+                </>
+              )}
+        </div>
+
+
       </div>
     );
   }
